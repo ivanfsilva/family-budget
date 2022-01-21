c6b240cf720d1208650622d83fe9b38c11c6ea69
ivanfsilva <ivanfs27@gmail.com>
2022-01-20 21:53:40 -0300

    added ReceitaRepository

---
+++ backend/src/main/java/br/com/ivanfsilva/familybudget/domain/repository/ReceitaRepository.java
@@ -0,0 +1,14 @@
+package br.com.ivanfsilva.familybudget.domain.repository;
+
+import br.com.ivanfsilva.familybudget.domain.model.orcamento.Receita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface ReceitaRepository extends JpaRepository<Receita, Long> {
+
+    Optional<Receita> findByDescricaoAndData(String desc, LocalDate data);
+}
